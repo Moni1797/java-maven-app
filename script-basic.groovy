4db367c62ec663ec74220dd6cf39bf1e0533891f
Moni1797 <munibawan@hotmail.com>
2026-09-23 08:08:15 +0100

    Add basic Groovy functions for build, test, and deploy

---
+++ script-basic.groovy
@@ -0,0 +1,10 @@
+def buildApp() {
+    echo "Groovy: building application"
+}
+def testApp() {
+    echo "Groovy: testing application"
+}
+def deployApp() {
+    echo "Groovy: deploying application"
+}
+return this
